74ce74d4992eed713f20ad37d53784d8f9bb0375
sunsetsobserver <adamlukawski@me.com>
2024-11-17 23:19:13 +0100

    Add length function to Registry and require name for register and clear functions

--- solidity/contracts/Registry.sol
+++ solidity/contracts/Registry.sol
@@ -7,6 +7,7 @@ import "./Concept.sol";
 contract Registry
 {
     mapping(string => address) private concepts;
+    uint private conceptCount; // Counter for the number of concepts
 
     event Received(address caller,  string message);
 
@@ -14,10 +15,10 @@ contract Registry
         emit Received(msg.sender, "Fallback was called");
     }
 
-    function register(string calldata name, Concept instance) external
-    {
-        require(this.contains(name) == false, string.concat(name, " concept of this name already registered"));
+    function register(string calldata name, Concept instance) external {
+        require(!this.contains(name), string.concat(name, " concept of this name already registered"));
         concepts[name] = address(instance);
+        conceptCount++; // Increment the counter
     }
 
     function at(string calldata name) external view returns (Concept)
@@ -25,13 +26,18 @@ contract Registry
         return Concept(concepts[name]);
     }
 
-    function clear(string calldata name) external
-    {
+    function clear(string calldata name) external {
+        require(this.contains(name), "Concept does not exist");
         concepts[name] = address(0);
+        conceptCount--; // Decrement the counter
     }
 
     function contains(string calldata name) external view returns (bool)
     {
         return concepts[name] != address(0);
+    }
+
+    function length() external view returns (uint) {
+        return conceptCount;
     }
 }
